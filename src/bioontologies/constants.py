8eb4f0ab0864dac8c6016a5eb80252da5e9213a9
Charles Tapley Hoyt <cthoyt@gmail.com>
2023-06-29 10:44:50 +0200

    Add curations
    - Add additional relation IRI curations
    - Add additional ontology IRIs

--- src/bioontologies/constants.py
+++ src/bioontologies/constants.py
@@ -3,6 +3,9 @@
 import bioregistry
 
 CANONICAL = {
+    "mamo": "http://identifiers.org/mamo",
+    "swo": "http://www.ebi.ac.uk/swo/swo.json",
+    "ito": "https://identifiers.org/ito:ontology",
     "apollosv": "http://purl.obolibrary.org/obo/apollo_sv.owl",
     "cheminf": "http://semanticchemistry.github.io/semanticchemistry/ontology/cheminf.owl",
     "dideo": "http://purl.obolibrary.org/obo/dideo/release/2022-06-14/dideo.owl",
