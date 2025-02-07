5ce2ad8c2b3be266f1258d6b1ac02373647854bd
Charles Tapley Hoyt <cthoyt@gmail.com>
2025-02-07 13:46:22 +0100

    Update NER utilities (#30)

--- src/bioontologies/__init__.py
+++ src/bioontologies/__init__.py
@@ -1,6 +1,7 @@
 """Tools for biomedical ontologies."""
 
 from .gilda_utils import get_gilda_terms
+from .ner import get_literal_mappings, get_literal_mappings_subset
 from .robot import (
     convert_to_obograph,
     get_obograph_by_iri,
@@ -11,6 +12,8 @@ from .robot import (
 __all__ = [
     "convert_to_obograph",
     "get_gilda_terms",
+    "get_literal_mappings",
+    "get_literal_mappings_subset",
     "get_obograph_by_iri",
     "get_obograph_by_path",
     "get_obograph_by_prefix",
